0f76c61bc4a7fd3b7d8cf21c7e5831c4a07fb137
Sandona soro <49130877+sandos1@users.noreply.github.com>
2021-01-06 18:58:34 -0500

    Update BatchUpdate_transaction.java

--- java_preactice/src/JDBC/BatchUpdate_transaction.java
+++ java_preactice/src/JDBC/BatchUpdate_transaction.java
@@ -17,7 +17,7 @@ public class BatchUpdate_transaction {
         ResultSet myRs = null;
         String myUrl="jdbc:mysql://localhost:3306/student";
         String user = "root";
-        String pwd="D@o09712130";
+        String pwd="root";
         try{
             // get connection to the database
         	try {
